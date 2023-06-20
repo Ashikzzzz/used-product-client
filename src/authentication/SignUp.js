df5518bb6efc7a954b6cae1b1805b342c4a222dc
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-06-20 14:56:49 +0600

    user signup successful

--- src/authentication/SignUp.js
+++ src/authentication/SignUp.js
@@ -1,9 +1,46 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import loginAnimataion from "../assets/animation/21372-launch-yourself.json"
 import Lottie from 'lottie-react';
 
 const SignUp = () => {
+
+    const navigate = useNavigate()
+
+    const handleSignUpSubmit =(event)=>{
+        event.preventDefault()
+        const form = event.target
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value 
+        const location = form.location.value 
+        const role = form.role.value 
+      const userData = {
+         name,
+        email,
+        password,
+        location,
+        role,
+        }
+
+        fetch("http://localhost:5000/api/v1/users/create-user",{
+            method:"POST",
+            headers : {
+                "Content-type":"application/json"
+            },
+            body: JSON.stringify(userData)
+        })
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            if(data?.status === "success"){
+                alert(data?.massage)
+                navigate("/")
+            }
+        })
+    }
+
+
   return (
     <div>
         <div>
@@ -14,7 +51,7 @@ const SignUp = () => {
                     <Lottie className='w-96 ml-28' animationData={loginAnimataion} loop={true} />
                 </div>
 
-             <form>
+             <form onSubmit={handleSignUpSubmit}>
              <div className='grid grid-cols-1'>
               <div className='mt-8'>
                 <label className="label">
@@ -53,7 +90,7 @@ const SignUp = () => {
               </div>
               <p className='font-normal text-sm mt-1'> Already Have an Account?<Link className='underline' to="/login"> Click here</Link> </p>
                <button type='submit' className="text-sm mt-6  w-1/2 mx-auto border-b-2  btn btn-primary ">
-                    <Link>Sign Up</Link>
+                    Sign Up
                </button>
               
              </form>
